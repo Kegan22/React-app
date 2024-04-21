0ffebeeb639b9f4c9171b6887adf85625e156e80
Kegan Akinsanmi <kegan2299@gmail.com>
2024-04-21 21:53:59 +0200

    props

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import ListGroup from "./Components/ListGroup";
 
 function App() {
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+
   return (
     <div>
-      <ListGroup />
+      <ListGroup items={items} heading="Cities" />
     </div>
   );
 }
